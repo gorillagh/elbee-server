4385ccb98b0fb9da88dd969d79b8e1cf3bf0add0
Albert <ron468896@gmail.com>
2022-12-03 08:56:39 +0000

    ffprobe fix5

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ mongoose
 
 //Middlewares
 app.use(morgan("tiny"));
-// app.use(cors());
+app.use(cors());
 app.use(express.json({ limit: "2mb" })); //Used to parse JSON bodies
 app.use(express.urlencoded({ limit: "2mb", extended: true })); //Parse URL-encoded bodies
 
