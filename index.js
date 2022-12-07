49f2665a147058b7881aa1ba9b7d9c47a2576e88
Albert <ron468896@gmail.com>
2022-12-07 15:06:51 +0000

    CORS fix 6

--- index.js
+++ index.js
@@ -22,38 +22,8 @@ mongoose
   );
 
 //Middlewares
-// app.use((req, res, next) => {
-//   req.header("Access-Control-Allow-Origin", "*");
-//   next();
-// });
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
-const handler = (req, res) => {
-  const d = new Date();
-  res.end(d.toString());
-};
-app.use(allowCors(handler));
-
 app.use(morgan("tiny"));
-// app.use(cors());
+app.use(cors());
 app.use(express.json({ limit: "2mb" })); //Used to parse JSON bodies
 app.use(express.urlencoded({ limit: "2mb", extended: true })); //Parse URL-encoded bodies
 
